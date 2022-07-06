266334ead29e2467ac01c57c7ee29fa45f6f8d95
LeivasGabi <leivasgabi@gmail.com>
2022-07-06 01:22:31 -0300

    criação de funções no back-end

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
+Route::apiResource("cliente", "api\ClienteController@index");
+Route::apiResource("pedido", "api\PedidoController@index");
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
